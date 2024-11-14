c3b1b606cfbb8d6e6a91e5ea13ef4236bcdf34a9
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-14 11:14:52 +0530

    add inital comment

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/nextjs-pages",
+  assetPrefix: "/nextjs-pages",
   env: {
     PASSWORD_PROTECT: process.env.NODE_ENV !== "development",
     SITE_CONFIG: process.env.ENV ?? "dev"
@@ -8,3 +16,5 @@ module.exports = {
     defaultLocale: "en",
   },
 };
+
+module.exports = nextConfig;
