afcbc504a1c2be88d0d11f25ea535f97cab25304
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-14 17:24:04 +0530

    update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: "akamai",
-    path: "/undp-digital-development-compass",
+    loader: "cloudinary",
+    path: "",
   },
   basePath: "/undp-digital-development-compass",
   assetPrefix: "/undp-digital-development-compass",
